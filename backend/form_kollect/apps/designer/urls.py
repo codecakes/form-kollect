897074da5d3f70e8c2efc1d94976a65c0622a85b
codecakes <akulmat@protonmail.com>
2020-06-24 01:59:19 +0530

    adding initial project work

---
+++ backend/form_kollect/apps/designer/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from form_kollect.apps.designer import views
+
+urlpatterns = [
+    path("index/", views.FormList.as_view(), name='designer-index'),
+    path("create/", views.create, name='designer-create'),
+    path("create_bulk_forms/", views.create_bulk_forms, name='designer-bulk-forms'),
+    path("show-bulk-forms/", views.show_bulk_forms, name="designer-show-bulk-forms"),
+]
---
+++ backend/form_kollect/asgi.py
@@ -0,0 +1,25 @@
+"""
+ASGI config for form_kollect project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'form_kollect.settings')
+
+django_application = get_asgi_application()
+
+
+async def application(scope, receive, send):
+    if scope["type"] == "http" or scope["type"] == "https":
+        await django_application(scope, receive, send)
+    # elif scope['type'] == 'websocket':
+    #     await websocket_application(scope, receive, send)
+    else:
+        raise NotImplementedError(f"Unknown scope type {scope['type']}")
